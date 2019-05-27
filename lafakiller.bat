f0e7286bed848e7f90ff69b345a007ea1dcbce00
Heizi <35397814+ElisaMin@users.noreply.github.com>
2019-05-27 21:05:53 +0800

    课间备份x2

--- lafakiller.bat
+++ lafakiller.bat
@@ -1,5 +1,5 @@
 @echo off
-title Laf杀手
+title LAF杀手
 mode con cols=80 lines=30
 color f0
 set rf=%~dp0
@@ -33,30 +33,40 @@ echo ---------------------------------------------------------------------------
 echo 目前已经识别到的设备有:
 %adb% devices
 echo -------------------------------------------------------------------------------
-echo 1.默认模式^(备份^+删除laf_a^)
-echo 2.高级模式^(可自行选择三天之内你撒A还是杀B^)
+echo 1. 默认模式^(备份^+删除laf_a^)
+echo 2. 高级模式^(可自行选择三天之内你撒A还是杀B^)
 echo -------------------------------------------------------------------------------
 choice /m "请选择模式(三秒后自动跳转默认)" /c 12 /d 1 /t 0003
 if %errorlevel%==1 goto easymode
 if %errorlevel%==2 goto hardmode
 :easymode
+title LAF杀手 -默认模式
 cls
 echo PowerBy-HEIZI %number%
 echo -------------------------------------------------------------------------------
 echo 注意: 请确保您的LG设备安装有Magisk并打开USB调试后连接电脑。
 echo -------------------------------------------------------------------------------
+echo 正在备份laf_a和laf_b到手机/sdcard/Download/lafBackup/中，本次备份码为"%number%"。  （备份码无用，只是为了防止名字重复，不需要特别记下来。）
+echo -------------------------------------------------------------------------------
+echo 正在备份laf_a和laf_b到%rf%
+echo -------------------------------------------------------------------------------
 goto end
+goto end-wrong
 :hardmode
+title LAF杀手 -高级模式
 cls
 echo PowerBy-HEIZI %number%
 echo -------------------------------------------------------------------------------
 echo 注意: 请确保您的LG设备安装有Magisk并打开USB调试后连接电脑。
 echo -------------------------------------------------------------------------------
 goto end
+goto end-wrong
 
+::test-backuplaf
 
-
-
+goto end-wrong
+:end-wrong
+set /p powerbyheizi=神奇BUG,提交后可获得一包七个小矮人。
 :end
 set /p powerbyheizi=执行完成,按任意键返回主页。
 goto home
