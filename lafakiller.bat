7da5408e9e22c27de168c296869b85541799b004
Heizi <35397814+ElisaMin@users.noreply.github.com>
2019-05-27 20:36:51 +0800

    备份

--- lafakiller.bat
+++ lafakiller.bat
@@ -1,36 +1,62 @@
 @echo off
-title="Laf杀手"
+title Laf杀手
+mode con cols=80 lines=30
 color f0
 set rf=%~dp0
+set number=%random%%random%_%time%
+cls
+echo PowerBy-HEIZI %number%
+echo -------------------------------------------------------------------------------
 :check-exist-adb.exe
 echo 检查adb.exe是否存在中……
+echo -------------------------------------------------------------------------------
 if not exist "%rf%adb\adb.exe" (
-echo.
 echo 错误:
 echo 未找到adb.exe，请下载完整包。
 goto r-a
 ) 
 echo 成功找到adb.exe
 set adb="%rf%adb\adb.exe"
+echo -------------------------------------------------------------------------------
 goto home
 :r-a
-echo 请将adb.exe拖入本窗口内
+echo -------------------------------------------------------------------------------
+echo 请自己准备的adb.exe拖入本窗口内，没有请关闭。
 set /p adb= 
 :home
 %adb% kill-server
-set number=%random%%random%_%time%
 cls
 echo PowerBy-HEIZI %number%
+echo -------------------------------------------------------------------------------
 echo 注意: 请确保您的LG设备安装有Magisk并打开USB调试后连接电脑。
-echo.
+echo -------------------------------------------------------------------------------
 echo 目前已经识别到的设备有:
 %adb% devices
-echo 请选择
+echo -------------------------------------------------------------------------------
 echo 1.默认模式^(备份^+删除laf_a^)
-echo 2.高级模式^(可自行选择撒A还是撒B^)
+echo 2.高级模式^(可自行选择三天之内你撒A还是杀B^)
+echo -------------------------------------------------------------------------------
+choice /m "请选择模式(三秒后自动跳转默认)" /c 12 /d 1 /t 0003
+if %errorlevel%==1 goto easymode
+if %errorlevel%==2 goto hardmode
+:easymode
+cls
+echo PowerBy-HEIZI %number%
+echo -------------------------------------------------------------------------------
+echo 注意: 请确保您的LG设备安装有Magisk并打开USB调试后连接电脑。
+echo -------------------------------------------------------------------------------
+goto end
+:hardmode
+cls
+echo PowerBy-HEIZI %number%
+echo -------------------------------------------------------------------------------
+echo 注意: 请确保您的LG设备安装有Magisk并打开USB调试后连接电脑。
+echo -------------------------------------------------------------------------------
+goto end
+
 
 
 
 :end
-set /p powerbyheizi=执行完成
+set /p powerbyheizi=执行完成,按任意键返回主页。
 goto home
