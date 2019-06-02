2627a51f6aa09ba8423ca8e740ce078af2410247
Heizi <35397814+ElisaMin@users.noreply.github.com>
2019-06-03 01:40:52 +0800

    v1.0-tested
    全部模式正常工作

--- lafakiller.bat
+++ lafakiller.bat
@@ -1,10 +1,9 @@
-
 @echo off
 title LAF杀手
 mode con cols=80 lines=30
 color f0
 set rf=%~dp0
-set number=%random%%random%_%time%
+set number=%random%%random%%random%%random%
 %rf%other/1.jpg
 cls
 echo PowerBy-HEIZI %number%
@@ -18,21 +17,27 @@ echo 未在目录下找到adb.exe，请下载完整包或重新定向adb.exe。
 goto r-a
 ) 
 echo 成功找到adb.exe
-set adb="%rf%adb\adb.exe"
+set adb=%rf%adb\adb.exe
 echo -------------------------------------------------------------------------------
-goto home
+goto pre-home
 :r-a
 echo -------------------------------------------------------------------------------
 echo 请自己准备的adb.exe拖入本窗口内，没有请关闭。
 set /p adb= 
-:home
+echo -------------------------------------------------------------------------------
+:pre-home
+echo 	初始化adb中
 %adb% kill-server
+%adb% start-server
+echo -------------------------------------------------------------------------------
+:home
 cls
 echo PowerBy-HEIZI %number%
 echo -------------------------------------------------------------------------------
 echo 注意: 请确保您的LG设备安装有Magisk并打开USB调试后连接电脑。
 echo -------------------------------------------------------------------------------
 echo 目前已经识别到的设备有:
+%adb% wait-for-device
 %adb% devices
 echo -------------------------------------------------------------------------------
 echo.
@@ -55,12 +60,25 @@ echo PowerBy-HEIZI %number%
 echo -------------------------------------------------------------------------------
 echo 注意: 请确保您的LG设备安装有Magisk并打开USB调试后连接电脑。
 echo -------------------------------------------------------------------------------
-echo 	检查root权限，请留意手机内的权限申请。
-%adb% shell "su -c 'echo %number%'"
+echo 	检查root权限中……请留意手机内的权限申请。
+for /f %%i in ('%adb% shell "su -c 'echo success'"') do set suckess=%%i 
+if not %suckess%==success (
+echo 	获取Root权限失败，请进入MagiskManager给予Shell Root权限后按回车键继续。
+echo 	没有Magisk请退出^(废话^)
+set /p powerbyheizi= 
+ for /f %%i in ('%adb% shell "su -c 'echo success'"') do set suckess=%%i 
+ if not %suckess%==success ( 
+ echo 	你还是没有授权Root权限，再见。
+ echo -------------------------------------------------------------------------------
+ goto end
+ )
+)
+echo 	获取Root权限成功
 echo -------------------------------------------------------------------------------
 echo 	正在备份laf_a和laf_b到手机/sdcard/Download/lafBackup/中……
-%adb% shell "su -c 'dd if=/dev/block/platform/soc/1d84000.ufshc/by-name/laf_a of=/sdcard/Download/lafBackup/laf_a_%number%.img'"
-%adb% shell "su -c 'dd if=/dev/block/platform/soc/1d84000.ufshc/by-name/laf_b of=/sdcard/Download/lafBackup/laf_b_%number%.img'"
+%adb% shell mkdir /sdcard/Download/lafBackup/
+%adb% shell "su -c 'cp /dev/block/bootdevice/by-name/laf_a /sdcard/Download/lafBackup/laf_a_%number%.img'"
+%adb% shell "su -c 'cp /dev/block/bootdevice/by-name/laf_b /sdcard/Download/lafBackup/laf_b_%number%.img'"
 echo -------------------------------------------------------------------------------
 echo 	正在复制备份文件到文件脚本目录lafBackup文件夹里……
 %adb% pull /sdcard/Download/lafBackup/laf_a_%number%.img %rf%lafBackup\laf_a_%number%.img
@@ -73,6 +91,7 @@ echo ---------------------------------------------------------------------------
 goto end
 goto end-wrong
 :hardmode
+set number=%random%%random%%random%%random%
 title LAF杀手 -高级模式
 cls
 echo PowerBy-HEIZI %number%
@@ -93,7 +112,13 @@ echo.
 echo.
 echo -------------------------------------------------------------------------------
 choice /m "请选择操作" /c 12456
-set number=%random%%random%_%date%%time%
+if %errorlevel%==1 set mode=1
+if %errorlevel%==2 set mode=2
+if %errorlevel%==3 set mode=3
+if %errorlevel%==4 set mode=4
+if %errorlevel%==5 set mode=5
+if %errorlevel%==6 set mode=6
+
 cls
 echo PowerBy-HEIZI %number%
 echo -------------------------------------------------------------------------------
@@ -102,23 +127,45 @@ echo ---------------------------------------------------------------------------
 echo 目前已经识别到的设备有:
 %adb% devices
 echo -------------------------------------------------------------------------------
-echo 	检查root权限，请留意手机内的权限申请。
-%adb% shell "su -c 'echo %number%'"
+echo 	检查root权限中……请留意手机内的权限申请。
+for /f %%i in ('%adb% shell "su -c 'echo success'"') do set suckess=%%i 
+if not %suckess%==success (
+echo 	获取Root权限失败，请进入MagiskManager给予Shell Root权限后再打开该程序。
+set /p powerbyheizi=看不懂上面我在说啥请自杀，按任意键立即自杀^(指立即退出程序^)。
+goto endd
+ )
+echo 	获取Root权限成功
 echo -------------------------------------------------------------------------------
-if %errorlevel%==1 (
+if %mode%==1 (
 set laf=laf_a
 goto laf-backup
 )
-if %errorlevel%==2 goto laf_b-backup
-if %errorlevel%==3 goto laf_a-delete
-if %errorlevel%==4 goto laf_b-delete
-if %errorlevel%==5 goto laf_a-restore
-if %errorlevel%==6 goto laf_b-restore
+if %mode%==2 (
+set laf=laf_b
+goto laf-backup
+)
+if %mode%==3 (
+set laf=laf_a
+goto laf-delete
+)
+if %mode%==4 (
+set laf=laf_b
+goto laf-delete
+)
+if %mode%==5 (
+set laf=laf_a
+goto laf-restore
+)
+if %mode%==6 (
+set laf=laf_b
+goto laf-restore
+)
 goto end
 goto end-wrong
 :laf-backup
 echo 	正在备份%laf%手机/sdcard/Download/lafBackup/中……
-%adb% shell "su -c 'dd if=/dev/block/platform/soc/1d84000.ufshc/by-name/%laf% of=/sdcard/Download/lafBackup/%laf%_%number%.img'"
+%adb% shell mkdir /sdcard/Download/lafBackup/
+%adb% shell "su -c 'dd if=/dev/block/bootdevice/by-name/%laf% of=/sdcard/Download/lafBackup/%laf%_%number%.img'"
 echo -------------------------------------------------------------------------------
 echo 	正在复制备份文件到文件脚本目录lafBackup文件夹里……
 %adb% pull /sdcard/Download/lafBackup/%laf%_%number%.img %rf%lafBackup\%laf%_%number%.img
@@ -140,60 +187,6 @@ set /p lafaf=
 echo -------------------------------------------------------------------------------
 goto end
 goto end-wrong
-
-:laf_a-backup
-echo 	正在备份laf_a手机/sdcard/Download/lafBackup/中……
-%adb% shell "su -c 'dd if=/dev/block/platform/soc/1d84000.ufshc/by-name/laf_a of=/sdcard/Download/lafBackup/laf_a_%number%.img'"
-echo -------------------------------------------------------------------------------
-echo 	正在复制备份文件到文件脚本目录lafBackup文件夹里……
-%adb% pull /sdcard/Download/lafBackup/laf_a_%number%.img %rf%lafBackup\laf_a_%number%.img
-echo -------------------------------------------------------------------------------
-goto end
-goto end-wrong
-:laf_b-backup
-echo 	正在备份laf_b手机/sdcard/Download/lafBackup/中……
-%adb% shell "su -c 'dd if=/dev/block/platform/soc/1d84000.ufshc/by-name/laf_b of=/sdcard/Download/lafBackup/laf_b_%number%.img'"
-echo -------------------------------------------------------------------------------
-echo 	正在复制备份文件到文件脚本目录lafBackup文件夹里……
-%adb% pull /sdcard/Download/lafBackup/laf_b_%number%.img %rf%lafBackup\laf_b_%number%.img
-echo -------------------------------------------------------------------------------
-goto end
-goto end-wrong
-:laf_a-delete
-echo 	正在删除laf_a……
-%adb% shell "su -c 'dd if=/dev/zero of=/sdcard/laf_a bs=512 count=98304 '"
-%adb% shell "su -c 'dd if=/sdcard/laf_a of=/dev/block/bootdevice/by-name/laf_a bs=512 count=98304 '"
-echo -------------------------------------------------------------------------------
-goto end
-goto end-wrong
-:laf_b-delete
-echo 	正在删除laf_b……
-%adb% shell "su -c 'dd if=/dev/zero of=/sdcard/laf_b bs=512 count=98304 '"
-%adb% shell "su -c 'dd if=/sdcard/laf_b of=/dev/block/bootdevice/by-name/laf_b bs=512 count=98304 '"
-echo -------------------------------------------------------------------------------
-goto end
-goto end-wrong
-:laf_a-restore
-echo 	请将要写入的laf_a文件拖入该窗口，本程序的备份文件在lafBackup文件夹内。
-set /p lafaf=
-%adb% push %lafaf% /sdcard/laf_a
-%adb% shell "su -c 'dd if=/sdcard/laf_a of=/dev/block/bootdevice/by-name/laf_a'"
-echo -------------------------------------------------------------------------------
-goto end
-goto end-wrong
-:laf_b-restore
-echo 	请将要写入的laf_b文件拖入该窗口，本程序的备份文件在lafBackup文件夹内。
-set /p lafbf=
-%adb% push %lafbf% /sdcard/laf_b
-%adb% shell "su -c 'dd if=/sdcard/laf_b of=/dev/block/bootdevice/by-name/laf_b'"
-echo -------------------------------------------------------------------------------
-goto end
-goto end-wrong
-
-set /p powerbyheizi=神奇BUG,提交后可获得一包七个小矮人。
-::test-backuplaf-and-change
-set /p powerbyheizi=神奇BUG,提交后可获得一包七个小矮人。
-::我想做一个灵活切换Laf_a Laf_b的
 goto end-wrong
 :end-wrong
 set /p powerbyheizi=神奇BUG,提交后可获得一包七个小矮人。
