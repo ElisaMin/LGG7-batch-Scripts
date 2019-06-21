944091be5dea01b15f34af53250c7efd9ad1ad5d
Heizi <35397814+ElisaMin@users.noreply.github.com>
2019-06-22 05:01:09 +0800

    Add files via upload

--- box-2.0.bat
+++ box-2.0.bat
@@ -1,11 +1,11 @@
-@echo on
+@echo off
 set rf=%~dp0
 title 懒人工具盒
 mode con cols=80 lines=30
 color f0
 set number=%random%%random%%random%%random%
 :start
-if exist %rf%lgup.off goto home
+if exist %rf%lgup.off goto home-check
 if exist %rf%lgup.on goto start-lgup-checks
 cls
 echo PowerBy-HEIZI 懒人工具盒 V2.0.%number% 
@@ -23,7 +23,7 @@ echo ---------------------------------------------------------------------------
 choice /m "请选择" /c 12
 echo powerbyheizi>lgup.off
 if %errorlevel%==1 goto start-lgup-check
-if %errorlevel%==2 goto home
+if %errorlevel%==2 goto home-check
 goto end
 goto end-wrong
 :start-lgup-check
@@ -119,13 +119,13 @@ echo PowerBy-HEIZI 懒人工具盒 V2.0.%number%
 echo -------------------------------------------------------------------------------
 echo 创建Common文件夹，并修改DLL中……
 if not exist "%ProgramFiles(x86)%\LG Electronics\LGUP\model\common\" md "%ProgramFiles(x86)%\LG Electronics\LGUP\model\common\"
-if errorlevel 0 ( 
+if not errorlevel 1 ( 
 echo ！错误！修改失败！正在跳转至帮助页面……
 echo -------------------------------------------------------------------------------
-%rf%file\other\help.url
+%rf%file\other\help.u
 goto lgup-end-error
 )
-copy -Y "%rf%file\lgup\LGUP_CommonD.dll" "%ProgramFiles(x86)%\LG Electronics\LGUP\model\common\LGUP_Common.dll"
+copy /Y "%rf%file\lgup\LGUP_CommonD.dll" "%ProgramFiles(x86)%\LG Electronics\LGUP\model\common\LGUP_Common.dll"
 echo -------------------------------------------------------------------------------
 echo 修改LGUP中……
 copy "%rf%file\lgup\LGUPD.exe" "%ProgramFiles(x86)%\LG Electronics\LGUP\LGUP.exe"
@@ -138,7 +138,7 @@ goto lgup-end-error
 echo -------------------------------------------------------------------------------
 echo 完成
 echo -------------------------------------------------------------------------------
-goto end
+goto lgup-end
 :setrec
 title 懒人工具盒 - 破解LGUP - 恢复模式
 cls
@@ -165,19 +165,83 @@ goto lgup-end-error
 echo -------------------------------------------------------------------------------
 echo 完成
 echo -------------------------------------------------------------------------------
-pause
-
+goto lgup-end
 :lgup-end-error
 title 错误！
 echo 清理状态中
 if exist %rf%lgup.off del %rf%lgup.off
 if exist %rf%lgup.on del %rf%lgup.on
 echo -------------------------------------------------------------------------------
-
 :lgup-end
 set /p powerbyheizi=点击任意键退出脚本
-goto edddddddddd
+exit /B 
+:home-check
+title 懒人工具盒 - 检测文件 adb 
+cls
+echo PowerBy-HEIZI 懒人工具盒 V2.0.%number% 
+echo -------------------------------------------------------------------------------
+echo 检查adb.exe是否存在中……
+echo -------------------------------------------------------------------------------
+if not exist "%rf%\file\adb\adb.exe" (
+echo ！错误！未检测到adb！请关闭程序把解压包解压到别的地方再启动本程序试试，如果不行请重新定向adb.exe，将任意的adb.exe拖入本程序窗口内后 按下回车键确认。
+echo -------------------------------------------------------------------------------
+set /p adb= 
+) else (
+echo 成功找到adb.exe
+set adb=%rf%file\adb\adb.exe
+echo -------------------------------------------------------------------------------
+)
+title 懒人工具盒 - 检测文件 fastboot
+echo 检查fastboot.exe是否存在中……
+echo -------------------------------------------------------------------------------
+if not exist "%rf%\file\adb\fastboot.exe" (
+echo ！错误！未检测到fastboot！请关闭程序把解压包解压到别的地方再启动本程序试试，如果不行请重新定向fastboot.exe，将任意的fastboot.exe拖入本程序窗口内后 按下回车键确认。
+echo -------------------------------------------------------------------------------
+set /p fastboot= 
+)
+echo 成功找到fastboot.exe
+set fastboot=%rf%file\adb\fastboot.exe
+echo -------------------------------------------------------------------------------
+echo 初始化adb中
+echo -------------------------------------------------------------------------------
+::%adb% kill-server
+::%adb% start-server
+echo -------------------------------------------------------------------------------
+
 :home 
+title 懒人工具盒 
+cls
+echo PowerBy-HEIZI 懒人工具盒 V2.0.%number% 
+echo -------------------------------------------------------------------------------
+echo 破解LGUP只出现一次，如果还想再次破解lgup可以删除在文件夹下的“lgup.off”文件。
+echo -------------------------------------------------------------------------------
+echo.
+echo.
+echo       1. 一键式优化官方系统 ^（禁用系统应用、时间同步、隐藏导航栏、重启。^）
+echo.
+echo       2. 刷入BOOT         3. 切换AB分区
+echo.
+echo       4. 刷入GSI          5. 隐藏菜单
+echo.
+echo.
+echo -------------------------------------------------------------------------------
+choice /m "请选择" /c 12
+if %errorlevel%==1 set mode=stock
+if %errorlevel%==2 set mode=boot
+echo -------------------------------------------------------------------------------
+for /f %%i in ('fastboot devices -l') do echo %%i>0
+if not exist 0 (
+echo 等待设备中……
+%fastboot% 
+) else (
+del 0
+echo 检测到设备"%%i" 
+)
+echo -------------------------------------------------------------------------------
+pause
+cls
+
+
 goto end
 goto end-wrong
 
